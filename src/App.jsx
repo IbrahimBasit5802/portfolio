fe3a023766180a66a44069f20e82e6e074d8a440
IbrahimBasit5802 <ibasit050802@gmail.com>
2023-03-10 14:10:03 +0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
       <BrowserRouter>
